f89dd4817ff48cbf4388f6b925f0a789039b190b
squarerobin <squarerobin.82@gmail.com>
2020-01-25 04:10:22 +0100

    Display Data from a Server API - done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./components/Header.js";
+import CharacterList from  "../src/components/CharacterList"
 
 
 export default function App() {
   return (
     <main>
       <Header />
+      <CharacterList />
     </main>
   );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,38 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
 
+  const [data, setData] = useState([]);
+
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios
+      .get("https://rickandmortyapi.com/api/character/")
+      .then(res => {
+        setData(res.data.results);
+      })
+      .catch(err => console.log(err));
   }, []);
+
+  console.log(data);
+  //console.log(error)
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      {/* TODO: `array.map()` over your state here! */}
+      {data.map(character => (
+        <div>
+          <h2>{character.name}</h2>
+          <p>Status: {character.status}</p>
+          <p>Species: {character.species}</p>
+          <p>Gender: {character.gender}</p>
+          <p>Origin: {character.origin.name}</p>
+          <p>Location: {character.location.name}</p>
+        </div>
+      ))}
     </section>
   );
 }
